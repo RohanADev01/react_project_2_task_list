1c73be8ae1001cb6038a8af1042c684fbfc29650
RohanA <rohanagarwal0192@gmail.com>
2021-12-24 19:08:59 +1100

    dynamic styling using template literals

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isValid, setIsValid] = useState(true);
+const CourseInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState("");
+    const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-    if (event.target.value.trim().length === 0) {
-      setIsValid(false);
-      return;
+    const goalInputChangeHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
+        setEnteredValue(event.target.value);
     };
-  };
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    if (enteredValue.trim().length === 0) {
-      setIsValid(false);
-      return;
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
     };
-    props.onAddGoal(enteredValue);
-  };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red':'black'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className={`form-control ${!isValid ? 'invalid': ''}`}>
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
